4a3e16cabd76030546f093604424a1c5424e4506
loudias88 <luisheleno.dias@gmail.com>
2024-03-18 13:49:19 -0300

    add_funcoes

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-print('Web Food\n')
+import os
 
-print('1. Cadastrar restaurante')
+def exibir_nome_do_programa():
+    print('Web Food\n')
 
-print('2. Listar restaurantes')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('3. Ativar restaurante')
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') 
+    print('Finalizando o app')
 
-print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
 
-opcao_escolhida = int(opcao_escolhida)
+    if opcao_escolhida == 1: 
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2: 
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3: 
+        print('Ativar restaurante')
+    else: 
+        finalizar_app()
 
-print(f'Você escolheu a opção {opcao_escolhida}')
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    print('Encerrando o programa')
+if __name__ == '__main__':
+    main()
