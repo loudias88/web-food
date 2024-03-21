9f18aaa0c880926f7f89ea46955116c4e62a5d6c
loudias88 <luisheleno.dias@gmail.com>
2024-03-21 13:57:56 -0300

    add try - except

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# web-food
+# Aplicativo Web Food
+
+## Descrição
+Este é um aplicativo de linha de comando simples chamado Web Food que permite aos usuários realizar diversas ações relacionadas à gestão de restaurantes.
+
+## Funcionalidades
+- **Cadastro de restaurante**: Permite aos usuários cadastrar um novo restaurante.
+- **Listar restaurantes**: Exibe uma lista de restaurantes cadastrados.
+- **Ativar restaurante**: Ativa um restaurante específico.
+- **Sair**: Encerra a aplicação.
+
+## Como começar
+1. Clone este repositório em sua máquina local.
+2. Certifique-se de ter o Python instalado.
+3. Execute o arquivo `main.py` em seu ambiente Python preferido.
+
+## Uso
+1. Ao executar o aplicativo, o menu principal será exibido.
+2. Escolha uma opção digitando o número correspondente e pressionando Enter.
+3. Siga as instruções para realizar a ação desejada.
+4. Para sair do aplicativo, selecione a opção "Sair".
--- app.py
+++ app.py
@@ -13,20 +13,31 @@ def finalizar_app():
     os.system('clear') 
     print('Finalizando o app')
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2: 
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3: 
-        print('Ativar restaurante')
-    else: 
-        finalizar_app()
+    try: 
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else: 
+            opcao_invalida() 
+    except: 
+        opcao_invalida()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
